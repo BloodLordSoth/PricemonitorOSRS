e2b1fa46cb29aeb143c186f260158e0b24e9f8a6
BloodLordSoth <allhailsoth@gmail.com>
2025-09-05 18:18:55 -0400

    Update push

---
+++ frontend/script.js
@@ -0,0 +1,50 @@
+const email = document.getElementById('email').value
+const dBones = document.getElementById('dBones')
+const scales = document.getElementById('scales')
+
+
+scales.addEventListener('change', () => {
+    scaleamt.style.display = scales.checked ? 'inline-block' : 'none'
+})
+
+dBones.addEventListener('change', () => {
+    dBoneamt.style.display = dBones.checked ? 'inline-block' : 'none'
+})
+
+async function submit(){
+    const dboneamt = document.getElementById('dBoneamt').value
+    const scaleamt = document.getElementById('scaleamt').value
+    const running = document.getElementById('running')
+    const arr = []
+    const scalearr = []
+    
+
+    if (scales.checked){
+        arr.push('Zulrah Scales')
+        scalearr.push(scaleamt)
+    } 
+
+    if (dBones.checked){
+        arr.push('dBones')
+    }
+
+    const dataObj = {
+        email: email,
+        scaleprice: scaleamt,
+        itemlist: arr,
+        dBonePrice: dboneamt
+    }
+
+    const res = await fetch('/run', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(dataObj)
+    })
+
+    if (!res.ok){
+        return window.alert('There was a server issue')
+    }
+    
+    console.log(`Email: ${email} Watching: ${arr}`)
+    running.style.display = 'block'
+}
--- server.js
+++ server.js
@@ -25,9 +25,11 @@ app.post('/run', async (req, res) => {
         if (itemlist.includes('Zulrah Scales')){
             await scrape('https://secure.runescape.com/m=itemdb_oldschool/Zulrah%27s+scales/viewitem?obj=12934', `${scaleprice}`, `${email}`)
         }
+
         if (itemlist.includes('dBones')){
             await scrape('https://secure.runescape.com/m=itemdb_oldschool/Dragon+bones/viewitem?obj=536', `${dBonePrice}`, `${email}`)
         }
+        
         res.sendStatus(200)
     }
     catch {
